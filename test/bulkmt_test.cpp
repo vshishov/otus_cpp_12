e22834aebe3a0c48927e2e25d6264280fc3f1f84
Vladimir Shishov <wladimir.shishov@gmail.com>
2020-07-02 00:17:34 +0300

    add advanced tests

--- test/bulkmt_test.cpp
+++ test/bulkmt_test.cpp
@@ -36,6 +36,39 @@ protected:
   std::stringstream ssMetricsLog;
 };
 
+class Bulkmt_2_Clinets_Test : public ::testing::Test
+{
+protected:
+  void Run(std::size_t a_nBlockSize, std::size_t a_szDelay = 1)
+  {      
+    auto pCommander = otus::Commander::Create("main", a_nBlockSize, ssMetricsMain);
+    auto pExecuter = otus::Executer::Create("log", pCommander, ssTestOut, ssMetricsLog);
+   
+    std::string strIdentifier_1 {"client_1"};
+    std::string strIdentifier_2 {"client_2"};
+
+    pCommander->Register(strIdentifier_1);
+    pCommander->Register(strIdentifier_2);
+
+    std::string strLine;
+    while ( std::getline(ssTestIn, strLine) ) {
+      pCommander->ProcessLine(strIdentifier_1, strLine);
+      pCommander->ProcessLine(strIdentifier_2, strLine);
+    }    
+
+    std::this_thread::sleep_for(std::chrono::seconds(a_szDelay));
+    
+    pCommander->Unregister(strIdentifier_1);
+    pCommander->Unregister(strIdentifier_2);
+  }
+
+protected:
+  std::stringstream ssTestIn;
+  std::stringstream ssTestOut;
+  std::stringstream ssMetricsMain;
+  std::stringstream ssMetricsLog;
+};
+
 TEST_F(Bulkmt_Test, empty_stream) {   
   Run(2);
 
@@ -79,7 +112,7 @@ TEST_F(Bulkmt_Test, single_block) {
     << "}" << std::endl
   ;
 
-  Run(3);
+  Run(3, 2);
   
   ASSERT_EQ(ssTestOut.str(), "bulk: cmd1, cmd2, cmd3, cmd4, cmd5\n");
   ASSERT_EQ(ssMetricsMain.str(), "main: 7 line(s), 1 block(s), 5 command(s)\n");
@@ -123,7 +156,7 @@ TEST_F(Bulkmt_Test, multi_block) {
     << "cmd1.4" << std::endl
     << "cmd1.5" << std::endl
     << "}" << std::endl
-    << "cmd3" << std::endl;
+    << "cmd3" << std::endl
   ;
 
   Run(3);
@@ -159,4 +192,67 @@ TEST_F(Bulkmt_Test, blocks_not_complete) {
   ASSERT_EQ(ssTestOut.str(), "bulk: cmd1, cmd2\n");
   ASSERT_EQ(ssMetricsMain.str(), "main: 15 line(s), 1 block(s), 2 command(s)\n");
   ASSERT_EQ(ssMetricsLog.str(), "log: 1 block(s), 2 command(s)\n");
+}
+
+TEST_F(Bulkmt_2_Clinets_Test, not_empty_stream) { 
+  ssTestIn << "cmd1" << std::endl
+    << "cmd2" << std::endl
+    << "cmd3" << std::endl
+  ;
+
+  Run(3);
+
+  ASSERT_EQ(ssTestOut.str(), "bulk: cmd1, cmd1, cmd2\nbulk: cmd2, cmd3, cmd3\n");
+  ASSERT_EQ(ssMetricsMain.str(), "main: 6 line(s), 2 block(s), 6 command(s)\n");
+  ASSERT_EQ(ssMetricsLog.str(), "log: 2 block(s), 6 command(s)\n");
+}
+
+TEST_F(Bulkmt_2_Clinets_Test, single_block) { 
+  ssTestIn << "{" << std::endl
+    << "cmd1" << std::endl
+    << "cmd2" << std::endl
+    << "cmd3" << std::endl
+    << "cmd4" << std::endl
+    << "cmd5" << std::endl
+    << "}" << std::endl
+  ;
+
+  Run(3, 3);
+  
+  ASSERT_EQ(ssTestOut.str(), "bulk: cmd1, cmd2, cmd3, cmd4, cmd5\nbulk: cmd1, cmd2, cmd3, cmd4, cmd5\n");
+  ASSERT_EQ(ssMetricsMain.str(), "main: 14 line(s), 2 block(s), 10 command(s)\n");
+  ASSERT_EQ(ssMetricsLog.str(), "log: 2 block(s), 10 command(s)\n");
+}
+
+TEST_F(Bulkmt_2_Clinets_Test, multi_block) { 
+  ssTestIn << "cmd1" << std::endl
+    << "cmd2" << std::endl
+    << "{" << std::endl
+    << "cmd1.1" << std::endl
+    << "cmd1.2" << std::endl
+    << "cmd1.3" << std::endl
+    << "{" << std::endl
+    << "cmd2.1" << std::endl
+    << "cmd2.2" << std::endl
+    << "cmd2.3" << std::endl
+    << "cmd2.4" << std::endl
+    << "cmd2.5" << std::endl
+    << "}" << std::endl
+    << "cmd1.4" << std::endl
+    << "cmd1.5" << std::endl
+    << "}" << std::endl
+  ;
+
+  Run(3, 4);
+
+  ASSERT_EQ(ssTestOut.str(), "bulk: cmd1, cmd1, cmd2\n"
+    "bulk: cmd1.1, cmd1.2, cmd1.3, "
+    "cmd2.1, cmd2.2, cmd2.3, cmd2.4, cmd2.5, "
+    "cmd1.4, cmd1.5\n"
+    "bulk: cmd1.1, cmd1.2, cmd1.3, "
+    "cmd2.1, cmd2.2, cmd2.3, cmd2.4, cmd2.5, "
+    "cmd1.4, cmd1.5\n"
+    "bulk: cmd2\n");
+  ASSERT_EQ(ssMetricsMain.str(), "main: 32 line(s), 4 block(s), 24 command(s)\n");
+  ASSERT_EQ(ssMetricsLog.str(), "log: 4 block(s), 24 command(s)\n");
 }
